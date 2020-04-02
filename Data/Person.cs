abcead52c61deb69b339654f0817fa071bdb0b82
echrs <echrs@outlook.com>
2020-04-02 23:36:45 +0200

    hitlist done

--- Data/Person.cs
+++ Data/Person.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System;
+using System.Collections.Generic;
 
 namespace PrijaviSusjeda.Data
 {
@@ -18,6 +19,25 @@ namespace PrijaviSusjeda.Data
         [Required] 
         [StringLength(5, MinimumLength = 5, ErrorMessage = "Poštanski broj mora imati minimalno 5 brojeva!")]
         public string PostanskiBroj { get; set; }
-        public string BrojNoge { get; set; }
+        [Range(1, 60, ErrorMessage = "To nije validan broj noge!")]
+        public int BrojNoge { get; set; }
+        public bool Rukavica { get; set; }
+        public bool Ruka { get; set; }
+        public bool Maska { get; set; }
+        public bool Kaslje { get; set; }
+        public bool Blid { get; set; }
+        public bool Samoizolacija { get; set; }
+        public bool Distanca { get; set; }
+        public int Rezultat { get; set; }
+        public void Kalkuliraj()
+        {
+            if (Rukavica) Rezultat++;
+            if (Ruka) Rezultat++;
+            if (Maska) Rezultat++;
+            if (Kaslje) Rezultat++;
+            if (Blid) Rezultat++;
+            if (Samoizolacija) Rezultat++;
+            if (Distanca) Rezultat++;
+        }
     }
 }
