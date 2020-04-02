82132b5e96d03d43c93d180063f869c1d4dca79a
echrs <echrs@outlook.com>
2020-04-02 23:55:34 +0200

    people.cs

---
+++ Data/People.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace PrijaviSusjeda.Data
+{
+    public class People
+    {
+        public static List<Person> people = new List<Person>();
+
+        public bool AddPerson(Person person)
+        {
+            people.Add(person);
+            return true;
+        }
+    }
+}
